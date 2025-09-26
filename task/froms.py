c1412cb1705d65e323c81d01e6afa7d53f196968
Abdul Ahad <ahad.cse25@gmail.com>
2025-09-26 18:40:22 +0600

    added widgets in froms.py and submit button design in the task_create.html

--- task/froms.py
+++ task/froms.py
@@ -5,3 +5,26 @@ class TaskFrom(forms.ModelForm):
     class Meta:
         model = Task
         exclude = ["user", "is_completed"]
+        
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-md mt-2 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400',
+                'placeholder': 'Enter Title Here'
+            }),
+            'discription': forms.TextInput(attrs={
+                'class': 'w-md mt-2 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400',
+                'placeholder': 'Enter Discription Here',
+                'min' : '0'
+            }),
+            'due_date': forms.DateInput(attrs={
+                'class': 'w-md mt-2 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400',
+                'placeholder': 'Enter Age Here',
+                'type' : 'date'
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-select px-4 py-2 rounded border border-gray-300 w-md',
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-select px-4 py-2 rounded border border-gray-300 w-md',
+            }),
+            }
